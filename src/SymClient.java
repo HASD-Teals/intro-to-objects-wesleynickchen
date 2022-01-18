7b281b00b8b165b2e0fbffc0c626fcf2bec05bbf
wesleynickchen <wesley.nickchen@gmail.com>
2022-01-18 08:43:44 -0600

    .

--- src/SymClient.java
+++ src/SymClient.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class SymClient {
     public static void main(String[] args) throws Exception {
         // Create a dog object with the default constructor
@@ -16,5 +18,52 @@ public class SymClient {
         System.out.println(myFirstDog.getName());
         System.out.println("Age: " + mySecondDog.getAge());
         System.out.println("Age in human years: " + mySecondDog.getHumanAge());
+        System.out.println();
+        ArrayList<Dog> myFirstHumansDogs = new ArrayList<Dog>();
+
+        System.out.println("this is a story about racism to spread awareness: \n");
+        Person mySecondPerson = new Person("Tyrone", "black", false, 19, null, "Store clerk");
+
+        Person myThridPerson = new Person("Kevin Stevens", "white", false, 34, null, "unemployed");
+        myThridPerson.becomeCop();
+
+        racism(mySecondPerson, myThridPerson);
+
+        waitOutPrison(mySecondPerson, 12);
+
+        murder(mySecondPerson, myThridPerson, true);
+
+        waitOutPrison(mySecondPerson, 50);
+
+        
+        mySecondPerson.printAge();
+
+        for (Dog dog : myFirstHumansDogs) {
+            System.out.println(dog);
+        }
     }
+
+    public static void murder(Person crim, Person victim, boolean caught){
+        System.out.println(victim.getName() + " has died");
+        victim = null;
+        if(caught){
+            System.out.println(crim.getName() + " was found guilty");
+            crim.setJailed(true);
+        }
+    }
+
+    public static void racism(Person criminalised, Person corruptCop){
+        if(!(criminalised.getRace() == corruptCop.getRace()) && corruptCop.getJob() == "cop"){
+            criminalised.setJailed(true);
+            System.out.println("racism has occured, " + criminalised.getName() + " is in jail!");
+        }
+    }
+
+    public static void waitOutPrison(Person crim, int sentance) {
+        System.out.println(crim.getName() + " has waited out prison");
+        crim.setJailed(false);
+        crim.setAge(crim.getAge() + sentance);
+    }
+
+
 }
